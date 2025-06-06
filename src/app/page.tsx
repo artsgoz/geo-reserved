4c1bac69147c20fc8f97001b966bb9ea378fabdc
Dhanavadh T. Saito <saito.teruyuki@outlook.jp>
2025-06-06 15:32:53 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,78 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-Noto_Sans_Thai)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center ">        
+        <div className="flex items-center justify-center gap-4 sm:gap-6">
+          <Image
+          className="w-20 h-20 sm:w-32 sm:h-32"
+          src="/geo_reserve.svg"
+          alt="GeoReserved logo"
+          width={128}
+          height={128}
           priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+          />
+          <div className="flex flex-col items-start">
+            <h1 className="text-2xl sm:text-4xl font-medium text-center sm:text-left">
+              GeoReserved
+            </h1>
+            <h1 className="text-md sm:text-lg font-medium text-center sm:text-left">
+              ระบบจองห้องคอมเอกภูมิศาสตร์
+            </h1>
+          </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4 items-center justify-center flex-row">
+            <label className="text-base sm:text-lg font-medium w-24" htmlFor="search">
+              รหัสนิสิต
+            </label>
+            <input
+            type="text"
+            placeholder="ป้อนรหัสนิสิต..."
+            className="w-full sm:w-[400px] h-10 sm:h-12 px-4 sm:px-5 border border-solid border-black/[.08] dark:border-white/[.145] rounded-full bg-transparent focus:outline-none focus:ring-2 focus:ring-neutral-800 transition-colors"
+            aria-label="Search for a room"
+            />          
+          </div>
+          <div className="flex gap-4 items-center justify-center flex-row">
+            <label className="text-base sm:text-lg font-medium w-24" htmlFor="search">
+              รหัสผ่าน
+            </label>
+            <input
+            type="password"
+            placeholder="ป้อนรหัสผ่าน..."
+            className="w-full sm:w-[400px] h-10 sm:h-12 px-4 sm:px-5 border border-solid border-black/[.08] dark:border-white/[.145] rounded-full bg-transparent focus:outline-none focus:ring-2 focus:ring-neutral-800 transition-colors"
+            aria-label="Search for a room"
+          />          
+          </div>
+        </div>
+        
+
+        <div className="flex gap-4 items-center flex-row">
+          <button
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-[140px] hover:cursor-pointer"
+            onClick={() => window.location.href = "#err"}
+            rel="noopener noreferrer"
+          >
+            เข้าสู่ระบบ
+          </button>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-[140px] hover:cursor-pointer"
+            href="https://www.arts.chula.ac.th/geography/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            เว็บไซต์หลัก
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <p>อยู่ในระหว่างการพัฒนา</p>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/artsgoz"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -65,37 +83,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          เอกสาร API Spec →
         </a>
       </footer>
     </div>
